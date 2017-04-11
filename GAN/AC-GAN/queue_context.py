4711b9e4ba6f768cc536db5272849b56e2715d0f
mxl1990 <mxl1990@gmail.com>
2017-04-11 16:02:41 +0800

    增加部分代码注释

--- GAN/AC-GAN/queue_context.py
+++ GAN/AC-GAN/queue_context.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import tensorflow as tf
 
 def queue_context(sess=None):
@@ -11,9 +12,13 @@ def queue_context(sess=None):
     """
 
     # default session
+    # 返回当前线程的默认会话
     sess = tf.get_default_session() if sess is None else sess
 
     # thread coordinator
+    # 创建多线程的协调器
     coord = tf.train.Coordinator()
+    # 在sess指定的会话，用coord指定的协调器启动所有queue_runner
+    # 返回所有线程列表
     threads = tf.train.start_queue_runners(sess=sess, coord=coord)
     return coord, threads
