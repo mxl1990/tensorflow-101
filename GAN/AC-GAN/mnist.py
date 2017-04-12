802fb1cab7b2f03e39f70c19b132410872f34646
mxl1990 <mxl1990@gmail.com>
2017-04-12 17:48:03 +0800

    增加部分注释，并删除无用参数

--- GAN/AC-GAN/mnist.py
+++ GAN/AC-GAN/mnist.py
@@ -1,5 +1,7 @@
+# -*- coding: utf-8 -*-
 from tensorflow.examples.tutorials.mnist import input_data
 from utils import data_to_tensor, Opt
+import globals
 
 
 
@@ -8,21 +10,32 @@ from utils import data_to_tensor, Opt
 class Mnist(object):
     r"""Downloads Mnist datasets and puts them in queues.
     """
-    _data_dir = './asset/data/mnist'
+    _data_dir = globals.mnist_dir
 
-    def __init__(self, batch_size=128, num_epochs=30, reshape=False, one_hot=False):
+    def __init__(self, batch_size=128, num_epochs=30, reshape=False):
 
         # load sg_data set
-        data_set = input_data.read_data_sets(Mnist._data_dir, reshape=reshape, one_hot=one_hot)
+        # one_hot参数无用，因而删减
+        # read_daat_sets定义如下，
+        # 只有fake_data为true的时候，one_hot参数才可用
+        # read_data_sets(train_dir,
+        #            fake_data=False,
+        #            one_hot=False,
+        #            dtype=dtypes.float32,
+        #            reshape=True,
+        #            validation_size=5000)
+        data_set = input_data.read_data_sets(Mnist._data_dir, reshape=reshape)
 
         self.batch_size = batch_size
 
         # save each sg_data set
         _train = data_set.train
+        # 验证集是直接使用训练集的前面validation_size个样本产生的
         _valid = data_set.validation
         _test = data_set.test
 
         # member initialize
+        # Opt计算特性为加法不覆盖，乘法覆盖
         self.train, self.valid, self.test = Opt(), Opt(), Opt()
 
         # convert to tensor queue
@@ -34,6 +47,7 @@ class Mnist(object):
             data_to_tensor([_test.images, _test.labels.astype('int32')], batch_size, name='test')
 
         # calc total batch count
+        # 计算总共的批处理的次数
         self.train.num_batch = _train.labels.shape[0] // batch_size
         self.valid.num_batch = _valid.labels.shape[0] // batch_size
         self.test.num_batch = _test.labels.shape[0] // batch_size
