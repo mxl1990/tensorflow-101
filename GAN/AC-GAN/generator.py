2e10879dca52b5e505562f39e101e653a378ddc0
mxl1990 <mxl1990@gmail.com>
2017-04-18 17:31:00 +0800

    更新variable_scope用法注释

--- GAN/AC-GAN/generator.py
+++ GAN/AC-GAN/generator.py
@@ -9,7 +9,9 @@ from tensorflow.python.ops import variable_scope
 def generator(tensor):
     reuse = len([t for t in tf.global_variables() if t.name.startswith('generator')]) > 0
     # print(tensor.get_shape())
-    #　用于生成上下文管理器，来管理变量名（感觉没用）
+    # 用于生成上下文管理器，来管理变量名
+    # 对应optimizer里面
+    # with里面所有创建的变量，其name属性都由generator开头
     with variable_scope.variable_scope('generator', reuse = reuse):
         # fully_connected函数定义在tensorflow.contrib.layers.python.layers中
         # 创建全连接神经网络，tensor为输入
