3147298a167df8d34b60139c9a6909258340d1ec
mxl1990 <mxl1990@gmail.com>
2017-04-21 19:08:34 +0800

    增加用于控制文件输出目录的全局变量信息

---
+++ GAN/Info-GAN/globals.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 存放全局变量
+# 主要用来控制生成存放文件和结果的目录
+# 并统一生成路径等
+
+# mnist图片下载目录路径
+global mnist_dir
+mnist_dir = './asset/data/mnist'
+
+# 存放中间文件的目录
+global midfile_dir
+midfile_dir = './asset/data/midfile/'
+
+# 文件前缀名
+global mid_prefix
+mid_prefix = "ac_gan"
+
+# 生成的图片文件目录
+global gen_image_dir
+gen_image_dir = "./asset/data/image/"
