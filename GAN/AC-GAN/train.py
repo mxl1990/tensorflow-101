91da34d204de4a3a9663ce2ae535b8550da3c4a8
mxl1990 <mxl1990@gmail.com>
2017-04-27 13:59:47 +0800

    修改目录确定方式，删除不必要的目录生成过程（已经移动到globals初始化中完成）

--- GAN/AC-GAN/train.py
+++ GAN/AC-GAN/train.py
@@ -153,11 +153,8 @@ with tf.Session() as sess:
                 cur_step = 0
                 # print('cur epoch {0} update l_d step {1}, loss_disc {2}, loss_gen {3}'.format(cur_epoch, l_d_step, l_disc, l_gen))
                 if cur_epoch % save_epoch == 0:
-                    # 检查保存目录
-                    import globals
-                    if not os.path.exists(globals.midfile_dir):
-                    	os.makedirs(globals.midfile_dir)
                     # 保存目录包括：目录路径和文件名前缀
+                    import globals
                     saver.save(sess, globals.midfile_dir + globals.mid_prefix, global_step=l_d_step)
     except tf.errors.OutOfRangeError:
         print('Train Finished')
